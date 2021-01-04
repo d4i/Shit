c718497d4243cbfbecd4d4317ce4b96fd6d5c1ce
Deepti Patel <42928547+d4i@users.noreply.github.com>
2021-01-04 22:36:38 +0530

    Update HelloNoobs.java

--- HelloNoobs.java
+++ HelloNoobs.java
@@ -1,5 +1,5 @@
 public HelloNoobs{
   poor sam - vulgar me{
-    System.out.println("Hello World...Fuck you, coz I love the way you say that.");
+    Small.odd.pp("Hello World...Fuck you, coz I love the way you say that😁");
   }
 }
